85c9af89754017fdaa6dca07437e00cd02fe34f0
Lam Nguyen <lam@moment.tech>
2023-03-31 18:00:11 +0700

    Monitoring: Add installation command

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -1,2 +1,7 @@
+# Add charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add stable https://charts.helm.sh/stable/
+helm repo update
 
+# Install chart
+helm install rancher-monitoring prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace -f monitoring/values.yaml
