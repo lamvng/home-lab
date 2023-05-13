9b6d417b19e4a561cb38ecbe8b067836966bffef
Lam Nguyen <lam@moment.tech>
2023-05-13 11:06:15 +0200

    feat: Refactor applications

--- setup/k3s/install.sh
+++ setup/k3s/install.sh
@@ -2,7 +2,7 @@
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik --disable servicelb" sh -
 
 # Copy kubeconfig file
-cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/k3s
+sudo cp /etc/rancher/k3s/k3s.yaml $HOME/.kube/k3s
 
 # Modify permission
 chown $USER:$USER $HOME/.kube/k3s
